3f8a402bdb7c66773b9cd03d901f1ecbf73fb38f
Trisfald <trisfald@gmail.com>
2016-09-08 20:24:30 +0200

    modified type generator with alias templates

--- examples/type_generator.cpp
+++ examples/type_generator.cpp
@@ -20,10 +20,19 @@ struct Dictionary
 	typedef std::unordered_map<Key, T, std::hash<Key>, std::equal_to<Key>, Alloc> type;
 };
 
+template <typename T>
+using Dictionary_cpp11 = std::unordered_map<std::string,
+		T,
+		std::hash<std::string>,
+		std::equal_to<std::string>,
+		My_alloc<std::pair<const std::string, T>>>;
+
 int main()
 {
 	Dictionary<std::string>::type str_dictionary; // a map of <string, string> with custom alloc
 	Dictionary<double>::type d_dictionary; // a map of <string, double> with custom alloc
+	Dictionary_cpp11<int> i_dictionary; // a map of <string, int> with custom alloc, c++11 alias templates
 	str_dictionary.insert({{"H", "house"}});
 	d_dictionary.insert({{"green", 0.8}});
+	i_dictionary.insert({{"Rome", -5}});
 }
