3b860e006ab70de77bbb8711dcbd3c8b73af0a7e
Andrea <Trisfald@users.noreply.github.com>
2017-12-05 17:17:21 +0100

    Update sfinae.cpp
    updated sfinae example with another syntax flavour

--- examples/sfinae.cpp
+++ examples/sfinae.cpp
@@ -8,89 +8,114 @@
 template <typename T>
 std::enable_if_t<std::is_integral<T>::value> foo(T t)
 {
-	std::cout << "integral call" << std::endl;
+    std::cout << "integral call" << std::endl;
 }
 
 template <typename T>
 std::enable_if_t<std::is_floating_point<T>::value> foo(T t)
 {
-	std::cout << "floating point call" << std::endl;
+    std::cout << "floating point call" << std::endl;
 }
 
 template <typename T>
 struct Check1
 {
-	template <typename U = T>
-	U read(typename std::enable_if_t<std::is_same<U, int>::value>* = nullptr)
-	{
-	   std::cout << "Check 1: type is int" << std::endl;
-	   return 1;
-	}
+    template <typename U = T>
+    U read(typename std::enable_if_t<std::is_same<U, int>::value>* = nullptr)
+    {
+        std::cout << "Check 1: type is int" << std::endl;
+        return 1;
+    }
 
-	template <typename U = T>
-	U read(typename std::enable_if_t<std::is_same<U, double>::value>* = nullptr)
-	{
-	   std::cout << "Check 1: type is double" << std::endl;
-	   return 1.0;
-	}
+    template <typename U = T>
+    U read(typename std::enable_if_t<std::is_same<U, double>::value>* = nullptr)
+    {
+        std::cout << "Check 1: type is double" << std::endl;
+        return 1.0;
+    }
 };
 
-// Preferred way to go
+// Second best
 template <typename T>
 struct Check2
 {
-	template <typename U = T, typename std::enable_if_t<std::is_same<U, int>::value>* = nullptr>
-	U read()
-	{
-	   std::cout << "Check 2: type is int" << std::endl;
-	   return 1;
-	}
+    template <typename U = T, typename std::enable_if_t<std::is_same<U, int>::value>* = nullptr>
+    U read()
+    {
+        std::cout << "Check 2: type is int" << std::endl;
+        return 1;
+    }
 
-	template <typename U = T, typename std::enable_if_t<std::is_same<U, double>::value>* = nullptr>
-	U read()
-	{
-	   std::cout << "Check 2: type is double" << std::endl;
-	   return 1.0;
-	}
+    template <typename U = T, typename std::enable_if_t<std::is_same<U, double>::value>* = nullptr>
+    U read()
+    {
+        std::cout << "Check 2: type is double" << std::endl;
+        return 1.0;
+    }
 };
 
 template <typename T>
 struct Check3
 {
-	template <typename U = T>
-	typename std::enable_if_t<std::is_same<U, int>::value, U> read()
-	{
-	   std::cout << "Check 3: type is int" << std::endl;
-	   return 1;
-	}
+    template <typename U = T>
+    typename std::enable_if_t<std::is_same<U, int>::value, U> read()
+    {
+        std::cout << "Check 3: type is int" << std::endl;
+        return 1;
+    }
 
-	template <typename U = T>
-	typename std::enable_if_t<std::is_same<U, double>::value, U> read()
-	{
-	   std::cout << "Check 3: type is double" << std::endl;
-	   return 1.0;
-	}
+    template <typename U = T>
+    typename std::enable_if_t<std::is_same<U, double>::value, U> read()
+    {
+        std::cout << "Check 3: type is double" << std::endl;
+        return 1.0;
+    }
+};
+
+// Preferred way to go
+template <typename T>
+struct Check4
+{
+    template <typename U = T, std::enable_if_t<std::is_same<U, int>::value, int> = 0>
+    U read()
+    {
+        std::cout << "Check 4: type is int" << std::endl;
+        return 1;
+    }
+
+
+    template <typename U = T, std::enable_if_t<std::is_same<U, double>::value, int> = 0>
+    U read()
+    {
+        std::cout << "Check 4: type is double" << std::endl;
+        return 1.0;
+    }    
 };
 
 int main()
 {
-	foo(1);
-	foo(2.0);
+    foo(1);
+    foo(2.0);
 
-	Check1<int> check1_int;
-	check1_int.read();
-	Check1<double> check1_double;
-	check1_double.read();
+    Check1<int> check1_int;
+    check1_int.read();
+    Check1<double> check1_double;
+    check1_double.read();
 
-	Check2<int> check2_int;
-	check2_int.read();
-	Check2<double> check2_double;
-	check2_double.read();
+    Check2<int> check2_int;
+    check2_int.read();
+    Check2<double> check2_double;
+    check2_double.read();
 
-	Check3<int> check3_int;
-	check3_int.read();
-	Check3<double> check3_double;
-	check3_double.read();
+    Check3<int> check3_int;
+    check3_int.read();
+    Check3<double> check3_double;
+    check3_double.read();
 
-	return 0;
+    Check4<int> check4_int;
+    check4_int.read();
+    Check4<double> check4_double;
+    check4_double.read();
+
+    return 0;
 }
